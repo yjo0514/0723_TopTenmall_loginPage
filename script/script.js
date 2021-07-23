e17f7fb1459a33c9a3d19b99194d4ee96437ac80
yjo0514 <yjo0514@naver.com>
2021-07-23 13:13:50 +0900

    0723_TopTenmall_loginPage

---
+++ script/script.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    //멀티탭 구현 시작
+
+    $("div#multiTabArea>div").click(function(){
+        $("div#multiTabArea>div").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    //멀티탭 구현 끝
+
+});
